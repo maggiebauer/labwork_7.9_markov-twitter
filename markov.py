6e50536f199858a55484cbc056d3ccaf785c6879
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-09 21:02:10 -0700

    unfinished version at end of class

--- markov.py
+++ markov.py
@@ -46,16 +46,16 @@ def make_text(chains):
 
     key = choice(chains.keys())
     words = [key[0], key[1]]
-    while key in chains:
+    while len(" ".join(words)) <= 140:    
+        while key in chains:
         # Keep looping until we have a key that isn't in the chains
         # (which would mean it was the end of our original text).
         #
         # Note that for long texts (like a full book), this might mean
         # it would run for a very long time.
-
-        word = choice(chains[key])
-        words.append(word)
-        key = (key[1], word)
+            word = choice(chains[key])
+            words.append(word)
+            key = (key[1], word)
 
     return " ".join(words)
 
@@ -66,8 +66,32 @@ def tweet(chains):
     # Use Python os.environ to get at environmental variables
     # Note: you must run `source secrets.sh` before running this file
     # to make sure these environmental variables are set.
+    api = twitter.Api(consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
+        consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
+        access_token_key=os.environ['TWITTER_ACCESS_TOKEN_KEY'],
+        access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
+    
+    print(api.VerifyCredentials())
 
-    pass
+    status = api.PostUpdate(chains)
+    print(status.text)   
+
+
+    tweeting = True
+    while tweeting:
+        keep_tweeting = input('Enter to tweet again [q to quit] > ')
+        if keep_tweeting == '\n':
+
+            print(api.VerifyCredentials())
+
+            status = api.PostUpdate(chains)
+            print(status.text)
+        elif keep_tweeting.lower().strip() == 'q':
+            tweeting = False
+        else:
+            print('I didn\'t understand that. Please try again.')
+            continue
+
 
 
 # Get the filenames from the user through a command line prompt, ex:
@@ -80,5 +104,8 @@ text = open_and_read_file(filenames)
 # Get a Markov chain
 chains = make_chains(text)
 
+
 # Your task is to write a new function tweet, that will take chains as input
 # tweet(chains)
+chains_2 = make_text(chains)
+tweet(chains_2)
